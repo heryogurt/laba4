03bac866cfedc4e454cff85bf451573013d9fb7e
heryogurt <92820593+heryogurt@users.noreply.github.com>
2024-10-14 03:47:23 +0300

    Delete projects/form/laba4_2.js

--- projects/form/laba4_2.js
+++
@@ -1,8 +0,0 @@
-btnElement = document.querySelector(".btn");
-resultElement = document.querySelector(".result");
-firstname = document.querySelector('[name="nname"]');
-secondname = document.querySelector('[name="ssurename"]')
-
-btnElement.addEventListener("click", function (){
-    resultElement.textContent = `Здравствуйте, ${firstname.value} ${secondname.value}!`
-});
